4afb1949153b8a1db6815799364c31707f9f8360
Carlos Perez <solracias@gmail.com>
2017-06-17 09:22:14 -0700

    started api routes. not done yet.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,26 @@
 // Your apiRoutes.js file should contain two routes:
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+// A GET route with the url /api/friends. This will 
+// be used to display a JSON of all possible friends.
+// A POST routes /api/friends. 
+// This will be used to handle incoming survey results. 
+// This route will also be used to handle the compatibility 
+// logic.
+module.exports = function(app, path){
 
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
+    app.get("/api/:friends?", function(req, res){
+        res.json(data.friends);
+    });
 
-// Sets up the Express App
-var app = express();
-var PORT = 3000;
+    app.post("/api/:friends?", function(req, res){
+        // var friends = req.params.characters;
+        // var friends = req.params.characters;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({
-    type: "application/vnd.api+json"
-}));
+        if(true){
+            //push
+        }
 
-app.get("/api/friends?", function(req, res){
-    var friends = req.params.characters;
-
-    if(friends){
-        console.log(friends);
-    }
-})
+        else{
+            //res.end();
+        }
+    })
+}

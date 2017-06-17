5c42db4f5bd29bb77f7261d06320072454feaf08
Carlos Perez <solracias@gmail.com>
2017-06-17 08:50:39 -0700

    added proper htmlroutes now using module.exports

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,29 @@
 // Your apiRoutes.js file should contain two routes:
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
+
+app.get("/api/friends?", function(req, res){
+    var friends = req.params.characters;
+
+    if(friends){
+        console.log(friends);
+    }
+})
